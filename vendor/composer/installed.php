376e107a814fa3cd34cc05152e48f1bbdd09faf2
Angel <git@agarcia.ga>
2021-04-02 03:00:35 -0600

    Primer release de front-page

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4dde43da50716056e1a8e8377a8a094db891691f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4dde43da50716056e1a8e8377a8a094db891691f',
+    ),
+  ),
+);
