b71fc4df240789b74cbc98bf49309f2a0d806283
Angel García <git@angelgarcia.dev>
2023-11-19 13:40:53 -0700

    Update index.js
    Cambio de dirección de correo.

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
 .forEach(i => i.classList.remove('nojs'))
 
 !(e => {
-  e.innerHTML = unescape('%32%61%78%69%63%61%7a%72%40%64%75%63%6b%2e%63%6f%6d')
+  e.innerHTML = unescape('%63%74%74%71%74%30%63%79%40%64%75%63%6b%2e%63%6f%6d')
   e.href = 'mailto:' + e.innerHTML
 })(document.querySelector('[href="mailto:honeypot@cia.gov"]'))
